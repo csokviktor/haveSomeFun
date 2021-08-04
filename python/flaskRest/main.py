f0c63b6d1f74a504509fcb96a2c8fe598874065d
Csók Viktor <csokviktor@sch.bme.hu>
2021-08-04 20:47:29 +0200

    full rest api

--- python/flaskRest/main.py
+++ python/flaskRest/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -12,42 +12,80 @@ db = SQLAlchemy(app)
 
 class VideoModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
 
+    def __repr__(self):
+        return f"Video(name = {name}, views = {views}, likes = {likes})"
 
-db.create_all()
+#db.create_all() for first db creation
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument('name', type=str, help='Name of video is required', required=True)
 video_put_args.add_argument('views', type=int, help='Views of video is required', required=True)
 video_put_args.add_argument('likes', type=int, help='Likes on video is required', required=True)
 
-videos = {}
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument('name', type=str, help='Name of video is required')
+video_update_args.add_argument('views', type=int, help='Views of video is required')
+video_update_args.add_argument('likes', type=int, help='Likes on video is required')
 
-def abortIfVideoIDNotExists(video_id):
-    if video_id not in videos:
-        abort(404, message="Video id is not valid")
 
-def abortIfVideoIDExists(video_id):
-    if video_id in videos:
-        abort(409, message=f'Video already exists with ID {video_id}')
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'views': fields.Integer,
+    'likes': fields.Integer
+}
+
 
 class Video(Resource):
+    @marshal_with(resource_fields) #serialize the return value with the dictionary
     def get(self, video_id):
-        abortIfVideoIDNotExists(video_id)
+        result = VideoModel.query.filter_by(id=video_id).first()
 
-        return videos[video_id]
+        if not result:
+            abort(404, message=f'Could not found video with {video_id} ID')
+
+        return result
     
+    @marshal_with(resource_fields)
     def put(self, video_id):
-        abortIfVideoIDExists(video_id)
-
         args = video_put_args.parse_args()
-        videos[video_id] = args
-        return videos[video_id], 201
-    
-    def delete(self, video_id):
-        abortIfVideoIDNotExists(video_id)
+        result = VideoModel.query.filter_by(id=video_id).first()
 
-        del videos[video_id]
+        if result:
+            abort(409, message=f"Video ID {video_id} taken.")
+
+        video = VideoModel(
+                id=video_id, name=args['name'],
+                views=args['views'], likes=args['likes']
+        )
+
+        db.session.commit()
+
+        return video, 201
+    
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+
+        if not result:
+            abort(404, message=f'Could not found video with {video_id} ID for update')
+
+        for arg in args.keys():
+            if args[arg]:
+                setattr(result, arg, args[arg])
+        
+        db.session.add(result)
+        db.session.commit()
+
+        return result
+
+    def delete(self, video_id):
+
         return {'message': 'Successful deletion'}, 204
 
         
--- python/flaskRest/test.py
+++ python/flaskRest/test.py
@@ -8,19 +8,20 @@ data = [
     {"likes": 10, 'name': 'Gyula', 'views': 10}
 ]
 
+for i in range(3):
+    res = requests.get(BASE + f"video/{i}")
+    print(res.json())
+
 for i in range(len(data)):
     res = requests.put(BASE + f"video/{i}", data[i])
     print(res.json())
     print(res.status_code)
 
 
-for i in range(len(data)):
-    res = requests.get(BASE + f'video/{i}')
-    print(res.json())
-    print(res.status_code)
+res = requests.patch(BASE + f"video/{2}", {'views': 1999})
+print(res.json())
 
-for i in range(len(data)):
-    res = requests.delete(BASE + f'video/{i}')
-    print(res.status_code)
-    print(res)
+for i in range(3):
+    res = requests.get(BASE + f"video/{i}")
+    print(res.json())
     
