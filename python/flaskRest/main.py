c68be3244849438ccc07743424ad81590adc41ab
Csók Viktor <csokviktor@sch.bme.hu>
2021-08-04 19:53:43 +0200

    restDB

--- python/flaskRest/main.py
+++ python/flaskRest/main.py
@@ -1,26 +1,56 @@
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
 api = Api(app)
+db = SQLAlchemy(app)
+
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+
+db.create_all()
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument('name', type=str, help='Name of video is required', required=True)
 video_put_args.add_argument('views', type=int, help='Views of video is required', required=True)
 video_put_args.add_argument('likes', type=int, help='Likes on video is required', required=True)
 
-videos = {
+videos = {}
 
-}
+def abortIfVideoIDNotExists(video_id):
+    if video_id not in videos:
+        abort(404, message="Video id is not valid")
 
+def abortIfVideoIDExists(video_id):
+    if video_id in videos:
+        abort(409, message=f'Video already exists with ID {video_id}')
 
 class Video(Resource):
     def get(self, video_id):
+        abortIfVideoIDNotExists(video_id)
+
         return videos[video_id]
     
     def put(self, video_id):
+        abortIfVideoIDExists(video_id)
+
         args = video_put_args.parse_args()
-        return {video_id: args}
+        videos[video_id] = args
+        return videos[video_id], 201
+    
+    def delete(self, video_id):
+        abortIfVideoIDNotExists(video_id)
+
+        del videos[video_id]
+        return {'message': 'Successful deletion'}, 204
+
+        
 
 api.add_resource(Video, "/video/<int:video_id>")
 
--- python/flaskRest/test.py
+++ python/flaskRest/test.py
@@ -2,6 +2,25 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-res = requests.put(BASE + "video/12", {"likes": 10})
+data = [
+    {"likes": 10, 'name': 'Viktor', 'views': 10},
+    {"likes": 10, 'name': 'Vitya', 'views': 101},
+    {"likes": 10, 'name': 'Gyula', 'views': 10}
+]
 
-print(res.json())
+for i in range(len(data)):
+    res = requests.put(BASE + f"video/{i}", data[i])
+    print(res.json())
+    print(res.status_code)
+
+
+for i in range(len(data)):
+    res = requests.get(BASE + f'video/{i}')
+    print(res.json())
+    print(res.status_code)
+
+for i in range(len(data)):
+    res = requests.delete(BASE + f'video/{i}')
+    print(res.status_code)
+    print(res)
+    
