ce19feb6a4c550e8e6cdf2b9b7d9ab111f78e16a
Viktor Csok <csokviktor@gmail.com>
2021-09-04 12:10:03 +0200

    code refactoring, continuing the course

--- go/webserviceAdv/ws/main.go
+++ go/webserviceAdv/ws/main.go
@@ -1,170 +1,18 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"strconv"
-	"strings"
+
+	"github.com/pluralsight/webservices/product"
 )
 
-type Product struct {
-	ProductID      int    `json:"productId"`
-	Manufacturer   string `json:"manufacturer"`
-	Sku            string `json:"sku"`
-	Upc            string `json:"upc"`
-	PricePerUnit   string `json:"pricePerUnit"`
-	QuantityOnHand int    `json:"quantityOnHand"`
-	ProductName    string `json:"productName"`
-}
+const basePath = "/api"
 
-func getNextID() int {
-	highestID := -1
-	for _, product := range productList {
-		if highestID < product.ProductID {
-			highestID = product.ProductID
-		}
-	}
-	return highestID + 1
-}
-
-func findProductByID(id int) (*Product, int) {
-	for i, product := range productList {
-		if product.ProductID == id {
-			return &product, i
-		}
-	}
-	return nil, 0
-}
-
-var productList []Product
-
-func init() {
-	productsJON := `[
-		{
-			"productId": 1,
-			"manufacturer": "Johns-Jenkins",
-			"sku": "asdqewqew",
-			"upc": "32eqweqwda",
-			"pricePerUnit": "234.1",
-			"quantityOnHand": 231231,
-			"productName": "sa"
-		},
-		{
-			"productId": 2,
-			"manufacturer": "Johns-Jenkins",
-			"sku": "asdqewqew",
-			"upc": "32eqweqwda",
-			"pricePerUnit": "234.1",
-			"quantityOnHand": 231231,
-			"productName": "nyika"
-		}
-	]`
-	err := json.Unmarshal([]byte(productsJON), &productList)
+func main() {
+	product.SetupRoutes(basePath)
+	err := http.ListenAndServe(":5000", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func productHandler(w http.ResponseWriter, r *http.Request) {
-	//get the id from url
-	urlPathSegment := strings.Split(r.URL.Path, "products/")
-	productID, err := strconv.Atoi(urlPathSegment[len(urlPathSegment)-1])
-	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-
-	//finds product with given id
-	product, listItemIndex := findProductByID(productID)
-	if product == nil {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-
-	switch r.Method {
-	case http.MethodGet:
-		//return
-		productJSON, err := json.Marshal(product)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(productJSON)
-	case http.MethodPut:
-		//update
-		var updatedProduct Product
-		bodyBytes, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		err = json.Unmarshal(bodyBytes, &updatedProduct)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		if updatedProduct.ProductID != productID {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		product = &updatedProduct
-		productList[listItemIndex] = *product
-		w.WriteHeader(http.StatusOK)
-		return
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
-	}
-
-}
-
-func productsHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		productsJson, err := json.Marshal(productList)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(productsJson)
-	case http.MethodPost:
-		var newProduct Product
-		bodyBytes, err := ioutil.ReadAll(r.Body)
-
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		err = json.Unmarshal(bodyBytes, &newProduct)
-
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		if newProduct.ProductID != 0 {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		newProduct.ProductID = getNextID()
-		productList = append(productList, newProduct)
-
-		w.WriteHeader(http.StatusCreated)
-		return
-	}
-
-}
-
-func main() {
-	http.HandleFunc("/products", productsHandler)
-	http.HandleFunc("/products/", productHandler)
-	http.ListenAndServe(":5000", nil)
 }
